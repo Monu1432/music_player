3cef58f9389bb607e1ae81a9fdc34061913eb3f8
Manoranjan Kumar Monu <96489228+Monu1432@users.noreply.github.com>
2023-04-21 12:45:58 +0530

    Update music.py

--- music.py
+++ music.py
@@ -1,12 +1,12 @@
-import tkinter as tk
+import tkinter as tk  #create your Gui using tkinter
 import fnmatch
 import os
 from pygame import mixer
 
-canvas = tk.Tk()
-canvas.title("Ms Music Player") #application name
-canvas.geometry("400x500") # application output screen size
-canvas.config(bg= 'black') # output background color of screen
+ms = tk.Tk()
+ms.title("Ms Music Player") #application name
+ms.geometry("400x500") # application output screen size
+ms.config(bg= 'black') # output background color of screen
 rootpath = "C:\\Users\MONU\Desktop\music file" #paste your music file path in this line
 pattern = "*.mp3" #play mp3 music 
 
@@ -54,29 +54,29 @@ def play_prev(): #play previous music
     
     
     
-listBox = tk.Listbox(canvas, fg= "cyan", bg="black", width= 100, font = ("poppins", 14)) 
+listBox = tk.Listbox(ms, fg= "cyan", bg="black", width= 100, font = ("poppins", 14)) 
 listBox.pack(padx=15, pady=15)
 
-label = tk.Label(canvas, text = "", bg="black", fg="yellow", font=('poppins', 18))
+label = tk.Label(ms, text = "", bg="black", fg="yellow", font=('poppins', 18))
 label.pack(pady = 15)
 
-top = tk.Frame(canvas, bg = "black")
+top = tk.Frame(ms, bg = "black")
 top.pack(padx= 10, pady= 5, anchor= "center")
 
-prevButton = tk.Button(canvas, text="Prev", image= prev_img, bg="black", borderwidth= 0, command= play_prev)
+prevButton = tk.Button(ms, text="Prev", image= prev_img, bg="black", borderwidth= 0, command= play_prev)
 prevButton.pack(pady=15, in_ = top, side = "left")
 
-startButton = tk.Button(canvas, text="Play", image= start_img, bg="black", borderwidth= 0, command = select)
+startButton = tk.Button(ms, text="Play", image= start_img, bg="black", borderwidth= 0, command = select)
 startButton.pack(pady=15, in_ = top, side = "left")
 
-pauseButton = tk.Button(canvas, text="stop", image= pause_img, bg="black", borderwidth= 0, command = stop)
+pauseButton = tk.Button(ms, text="stop", image= pause_img, bg="black", borderwidth= 0, command = stop)
 pauseButton.pack(pady=15, in_ = top, side = "left")
 
-nextButton = tk.Button(canvas, text="Next", image= naxt_img, bg="black", borderwidth= 0, command=  play_next)
+nextButton = tk.Button(ms, text="Next", image= naxt_img, bg="black", borderwidth= 0, command=  play_next)
 nextButton.pack(pady=15, in_ = top, side = "left")
 
 for root, dirs, files in os.walk(rootpath): # find the directories your given music file in your pc
 
     for filename in fnmatch.filter(files, pattern):
         listBox.insert("end", filename)
-canvas.mainloop()
+ms.mainloop()
